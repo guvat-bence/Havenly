af03ea878f16c2f1ba66c300c8691397c54577a0
HMate0317 <mateherczeg740@gmail.com>
2025-11-12 09:50:26 +0100

    web

---
+++ web/resources/js/lib/utils.ts
@@ -0,0 +1,16 @@
+import { InertiaLinkProps } from '@inertiajs/vue3';
+import { clsx, type ClassValue } from 'clsx';
+
+export function cn(...inputs: ClassValue[]) {
+}
+
+export function urlIsActive(
+    urlToCheck: NonNullable<InertiaLinkProps['href']>,
+    currentUrl: string,
+) {
+    return toUrl(urlToCheck) === currentUrl;
+}
+
+export function toUrl(href: NonNullable<InertiaLinkProps['href']>) {
+    return typeof href === 'string' ? href : href?.url;
+}
---
+++ web/resources/js/pages/Welcome.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1 class="fasz">hello</h1>
+</template>
+
+<style scoped>
+    .fasz {
+        color: brown
+    }
+</style>
