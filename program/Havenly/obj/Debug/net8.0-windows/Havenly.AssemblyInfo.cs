7c150804d7ad9fda2ca810e2024c949cd675fa37
HMate0317 <mateherczeg740@gmail.com>
2025-11-21 08:37:31 +0100

    remove window

--- program/Havenly/obj/Debug/net8.0-windows/Havenly.AssemblyInfo.cs
+++ program/Havenly/obj/Debug/net8.0-windows/Havenly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Havenly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdb587fae54e014f98920c9010c00bddee59600c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ae09091c86f2011528482808ec580ddb90a6235")]
 [assembly: System.Reflection.AssemblyProductAttribute("Havenly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Havenly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
