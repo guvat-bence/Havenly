b4297b3ac580c251067076091b1d69276a87885c
HMate0317 <mateherczeg740@gmail.com>
2025-11-26 10:15:35 +0100

    add headlessui add heroicons

--- web/frontend/src/App.vue
+++ web/frontend/src/App.vue
@@ -1,9 +1,9 @@
-<script setup></script>
+<script setup>
+import navbar from './components/navbar.vue';
+</script>
 
 <template>
-  <h1 class="text-center text-6xl font-bold">
-    Hello world!
-  </h1>
+   <navbar></navbar>
 </template>
 
 <style scoped></style>
