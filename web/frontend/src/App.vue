5b2bc61bea72e2b2d6a0fcbfb860fa48818c6a6c
HMate0317 <mateherczeg740@gmail.com>
2025-12-08 18:05:36 +0100

    setup base

--- web/frontend/src/App.vue
+++ web/frontend/src/App.vue
@@ -4,8 +4,16 @@ import Navbar from './components/navbar.vue';
 </script>
 
 <template>
-  <Navbar></Navbar>
-  <RouterView></RouterView>
+<div class="page-container d-flex 
+            flex-column min-vh-100 
+            vw-100 overflow-x-hidden">
+    <navbar class="sticky-top"/>
+    <router-view class="flex-fill"/>
+</div>
 </template>
 
-<style scoped></style>
+<style>
+body{
+  background-color: rgb(34, 34, 34) !important;
+}
+</style>
