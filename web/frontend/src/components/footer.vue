a0329e2b717476b976b25798717e849fc0526db2
guvat-bence <guvat.bence-2021@keri.mako.hu>
2025-12-09 13:27:07 +0100

    footer texts.

--- web/frontend/src/components/footer.vue
+++ web/frontend/src/components/footer.vue
@@ -2,16 +2,20 @@
   <footer class="bg-dark">
     <div class="row text-white text-center justify-content-center mt-3">
       <section class="col-4">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam reiciendis, possimus illum suscipit veritatis natus totam nemo quod deserunt unde. Ab veritatis consequatur dolor excepturi et possimus facilis praesentium perspiciatis.
-        </p>
+        <h3>Havenly</h3>
+        <p>Az élmények határtalanok, az otthon érzése pedig mindig veled tart.</p>
+      </section>
+       <section class="col-4">
+        <h3>Kövess minket:</h3>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
       </section>
       <section class="col-4">
+        <h3>Elérhetőségeink:</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat incidunt quisquam voluptatibus. Amet sed dolorum ea nisi porro fugiat repellat, corrupti, ipsum consequatur accusantium architecto quos culpa quae adipisci consectetur?
         </p>  
-      </section>
-      <section class="col-4">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque est fugiat alias! Itaque eum quae quis voluptates. Vel deserunt ea nemo obcaecati enim iure quis architecto repudiandae at, alias quaerat!
-        </p>
       </section>
     </div>
   </footer>
