60a0c6059d0ab11947044a94d69109fa2fb1d935
guvat-bence <guvat.bence-2021@keri.mako.hu>
2025-12-10 09:41:09 +0100

    database setted up to the vue pages.

--- web/frontend/src/pages/accommodation.vue
+++ web/frontend/src/pages/accommodation.vue
@@ -1,62 +1,33 @@
 <script setup>
-let accommodations = [
-	{
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-    {
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-    {
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-    {
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-    {
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-    {
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-    {
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-    {
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-    {
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-    {
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-    {
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-    {
-		name: "Szállások",
-		path: "../src/image/image.png"
-	},
-];
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const accommodations = ref([]);
+
+onMounted(()=>
+{
+	axios.get("http://localhost:3000/apartmans")
+		.then(response => 
+		{
+			accommodations.value = response.data
+		})
+		.catch(error=>{console.error(error)})
+		
+})
+
+console.log(accommodations);
+
 </script>
 <template>
     <div class="container">
         <div class="row justify-content-center mt-5">
-            <div v-for="x in accommodations" 
+            <div v-for="x in accommodations" x:key="apartman_id" 
                  class="card mx-2 my-2 text-nowrap 
-                        col-8 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2">
-                <img :src="x.path" class="bg-dark mx-auto col-12">
+                        col-8 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <!-- <img :src="x.path" class="bg-dark mx-auto col-12"> -->
+								<div class="card-body">
+									<p class="text-center">{{x.apartman_name}}</p>
+								</div>
             </div>
         </div>
     </div>
--- web/frontend/src/pages/home.vue
+++ web/frontend/src/pages/home.vue
@@ -4,8 +4,10 @@
       <h1 class="display-1 text-white text-center w-100 mt-3">
         Hová szeretnél utazni?
       </h1>
-      <form class="form mt-5 col-3 row justify-content-center bg-dark bg-opacity-50 rounded-3 shadow">
-        <input  class="form-control mt-2" type="search" name="searchbar" id="searchbar">
+      <form class="form mt-5 col-3 row justify-content-center 
+                   bg-dark bg-opacity-50 rounded-3 shadow">
+        <input  class="form-control mt-2" type="search" 
+                name="searchbar" id="searchbar">
         <button class="btn btn-secondary col-11 my-2">Keresés</button>
       </form>
     </div>
