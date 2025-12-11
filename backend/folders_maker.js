3d22df02e54ae3989dfab3bffbe0c22593919b19
guvat-bence <guvat.bence-2021@keri.mako.hu>
2025-12-11 13:41:35 +0100

    modify folder_maker

--- backend/folders_maker.js
+++ backend/folders_maker.js
@@ -7,18 +7,23 @@ const db = mysql.createConnection({
   host:"localhost",
   user:"root",
   password:"",
-  database:"havanly"
+  database:"havenly"
 })
 
 db.connect(err=>{
   if(err)
     {
-      console.error("Hiba a Mysql szerver csatlakozáskor:"err);
+      console.error("Hiba a Mysql szerver csatlakozáskor:",err);
       return;
     }
     console.log("Sikeres csatlakozás!");
 })
 
-app.listen(port()=>{
+app.listen(port,()=>{
   console.log(`folders_maker is running on port ${port}`);
+})
+
+app.get('/datas',(req,res,next)=>
+{
+  db.query()
 })
