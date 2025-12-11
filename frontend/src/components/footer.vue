a0f6c552417e896875952a9b5d9133926771f0b3
guvat-bence <guvat.bence-2021@keri.mako.hu>
2025-12-11 11:27:06 +0100

    footer finished.

--- frontend/src/components/footer.vue
+++ frontend/src/components/footer.vue
@@ -25,29 +25,27 @@
       <section class="col-12 col-sm-6 col-md-4 
                       col-lg-4 col-xl-6 col-xxl-6 mb-5 mb-sm-4 mb-md-0">
         <h3>Havenly</h3>
-        <p>Az élmények határtalanok, az otthon érzése pedig mindig veled tart.</p>
-        <hr>
-        <a class="nav-link mt-lg-4" href="">Adatvédelem</a>
-        <a class="nav-link mt-lg-4" href="">Felhasználói feltételek</a>
+        <p class="mb-sm-3 mb-md-4 mb-lg-5">Az élmények határtalanok, az otthon érzése pedig mindig veled tart.</p>
+        <a class="nav-link mt-1 mt-sm-2 mt-lg-4" href="">Adatvédelem</a>
+        <a class="nav-link mt-1 mt-sm-2 mt-lg-4" href="">Felhasználói feltételek</a>
       </section>
       <section class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3 ">
         <h3>Elérhetőségeink:</h3>
-        <a class="nav-link mb-1" 
+        <a class="nav-link mb-1 mb-1 mt-md-3" 
            href="mailto:guvat.bence-2021@keri.mako.hu">
             <font-awesome-icon icon="fa-solid fa-envelope" size="xl"/>
              havenly@gmail.com
         </a>
-        <a class="nav-link mb-1" 
+        <a class="nav-link mb-1 mb-1 mt-md-3" 
            href="tel:+36304583453">
             <font-awesome-icon icon="fa-solid fa-phone" size="xl"/> 
              +36303425678
         </a>
-        <a class="nav-link mb-1" 
+        <a class="nav-link mb-1 mb-1 mt-md-3" 
            href="https://www.google.com/maps/@46.9096969,19.683835,3a,77.1y,213.2h,91.18t/data=!3m7!1e1!3m5!1sJGuIwHNr92f18NEz_5t74A!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%ű2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D-1.1802603712099682%26panoid%3DJGuIwHNr92f18NEz_5t74A%26yaw%3D213.20307359600648!7i16384!8i8192?entry=ttu&g_ep=EgoyMDI1MTIwOC4wIKXMDSoKLDEwMDc5MjA3MUgBUAM%3D">
             <font-awesome-icon icon="fa-solid fa-location-dot" size="xl"/>
              Kecskemét, József Attila u. 5
         </a>
-        <hr>
         <div class="d-flex justify-content-center align-items-center mt-4">
           <a class="nav-link" 
              href="">
