e25f9fffd3edfb80c68fe73195c93a82a9624993
guvat-bence <guvat.bence-2021@keri.mako.hu>
2025-12-12 09:45:14 +0100

    folders_maker.js edited.

--- backend/folders_maker.js
+++ backend/folders_maker.js
@@ -1,47 +1,51 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const fs = require("node:fs");
 const port = 3100;
 
 const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"",
-  database:"havenly"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "havenly"
 })
 
-db.connect(err=>{
-  if(err)
-    {
-      console.error("Hiba a Mysql szerver csatlakozáskor:",err);
-      return;
-    }
-    console.log("Sikeres csatlakozás!");
+db.connect(err => {
+  if (err) {
+    console.error("Hiba a Mysql szerver csatlakozáskor:", err);
+    return;
+  }
+  console.log("Sikeres csatlakozás!");
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`folders_maker is running on port ${port}`);
 })
 
-app.get('/datas',(req,res)=>
-{
-  db.query("SELECT `countries`.`name` AS `country_name`," + 
-                   "`cities`.`name` AS `city_name`," + 
-                   "`accommodations`.`name` AS `accommodation_name`," + 
-                   "`experiences`.`name` AS `experience_name` FROM `countries`" +
-           "INNER JOIN `cities`" +
-           "ON `cities`.`country_id` = `countries`.`id`" +
-           "INNER JOIN `accommodations`" +
-           "ON `cities`.`id` = `accommodations`.`city_id`" +
-           "LEFT JOIN `experiences`" +
-           "ON `cities`.`id` = `experiences`.`city_id`",(err, result) =>
-            {
-               if(err){
-                console.error("Hiba a apartmans beolvasásakor", err);
-                res.status(500).send("Adatbázis hiba");
-                return;
-              }
-              res.json(result);
-            })
-  
-})
+let datas = [];
+
+db.query(
+  "SELECT `countries`.`name` AS `country_name`," +
+  "`cities`.`name` AS `city_name`," +
+  "`accommodations`.`name` AS `accommodation_name`," +
+  "`experiences`.`name` AS `experience_name` FROM `countries`" +
+  "INNER JOIN `cities`" +
+  "ON `cities`.`country_id` = `countries`.`id`" +
+  "INNER JOIN `accommodations`" +
+  "ON `cities`.`id` = `accommodations`.`city_id`" +
+  "LEFT JOIN `experiences`" +
+  "ON `cities`.`id` = `experiences`.`city_id`", 
+  (err, result) => {
+    if (err) {
+      console.error("Hiba a apartmans beolvasásakor", err);
+      return;
+    }
+    
+    datas = result;
+    console.log(datas);
+
+    
+  }
+)
+
