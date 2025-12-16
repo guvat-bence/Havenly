90209dedf2ef46bb36e80db393f55b423a809456
HMate0317 <mateherczeg740@gmail.com>
2025-12-16 12:07:43 +0100

    added display text

--- frontend/src/pages/experience.vue
+++ frontend/src/pages/experience.vue
@@ -4,5 +4,6 @@ import Cards from '@/components/cards.vue';
 </script>
 
 <template>
+	<h1 class="text-center text-white display-1">Élmények</h1>
   <Cards tableName="experiences" :country="3"/>
 </template>
