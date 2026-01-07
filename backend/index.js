c4e31c09f2fa1b94c31ec849b59b77713c50d4eb
guvat-bence <guvat.bence-2021@keri.mako.hu>
2026-01-07 14:07:52 +0100

    working on home.vue

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
 // Adatbázis csatlakozás
 const db = mysql.createConnection({
   host: "localhost",
@@ -25,6 +26,7 @@ db.connect((err) => {
 app.listen(port, () => {
   console.log(`index.js is running on port ${port}`);
 });
+
 // Szállások le kérdezése
 app.get('/accommodations', (req, res) => {
   db.query('SELECT `accommodations`.`id`, `accommodations`.`owner_id`,'+
@@ -44,6 +46,51 @@ app.get('/accommodations', (req, res) => {
     res.json(result);
   })
 })
+
+//random generáljon country id-t és írja ki a hozzá rendelt szállásokat
+app.get("/accommodations/randCountryID", (req, res) => {
+  db.query(
+    `SELECT DISTINCT accommodations.country_id,
+                            countries.name AS 'country_name' 
+            FROM accommodations 
+            INNER JOIN countries 
+            ON accommodations.country_id = countries.id
+            ORDER BY RAND() LIMIT 5`,
+    (err, result) => {
+      if (err) {
+        console.error("Hiba az accommodations beolvasásakor", err);
+        res.status(500).send("Adatbázis hiba");
+        return;
+      }
+      res.json(result);
+    }
+  );
+});
+
+// A top 5 leglátogatottabb szállás, le kérdezése
+app.get('/accommodations/top5', (req, res) => {
+  db.query('SELECT COUNT(`history`.`id`) AS `rented_times`,'+
+            '`accommodations`.`id`,'+
+            '`accommodations`.`country_id`,'+
+            '`countries`.`name` '+
+           'FROM `history` '+
+           'INNER JOIN `accommodations` '+
+           'ON `history`.`accommodation_id` = `accommodations`.`id` '+
+           'INNER JOIN `countries` '+
+           'ON `accommodations`.`country_id` =  `countries`.`id` '+
+           'GROUP BY `accommodations`.`id` ' +
+           'HAVING COUNT(`history`.`accommodation_id`) > 1 ' +
+           'ORDER BY COUNT(`history`.`accommodation_id`)DESC '+
+           'LIMIT 5', (err,result) => {
+    if(err){
+      console.error("Hiba a accommodations beolvasásakor", err);
+      res.status(500).send("Adatbázis hiba");
+      return;
+    }
+    res.json(result);
+  })
+})
+
 // Élmények le kérdezése
 app.get("/experiences", (req, res) => {
   db.query(
@@ -57,25 +104,6 @@ app.get("/experiences", (req, res) => {
     (err, result) => {
       if (err) {
         console.error("Hiba a experiences beolvasásakor", err);
-        res.status(500).send("Adatbázis hiba");
-        return;
-      }
-      res.json(result);
-    }
-  );
-});
-//random generáljon country id-t és írja ki a hozzá rendelt szállásokat
-app.get("/accommodations/randCountryID", (req, res) => {
-  db.query(
-    `SELECT DISTINCT accommodations.country_id,
-                            countries.name AS 'country_name' 
-            FROM accommodations 
-            INNER JOIN countries 
-            ON accommodations.country_id = countries.id
-            ORDER BY RAND() LIMIT 5`,
-    (err, result) => {
-      if (err) {
-        console.error("Hiba az accommodations beolvasásakor", err);
         res.status(500).send("Adatbázis hiba");
         return;
       }
@@ -167,13 +195,3 @@ app.post('/register', (req, res) => {
     }
   );
 });
-
-// Ez kell a kezdő laphoz, holnap folytatom....:}
-// SELECT `history`.`accommodation_id` , COUNT(*) as `rented_times`, `accommodations`.`id`,  `accommodations`.`owner_id`, `accommodations`.`country_id`, `accommodations`.`city_id`, `accommodations`.`name`, `accommodations`.`folder_name`
-// FROM `history`
-// INNER JOIN `accommodations`
-// ON `history`.`accommodation_id` = `accommodations`.`id`
-// GROUP BY `history`.`accommodation_id`
-// HAVING COUNT(*)>1
-// ORDER BY COUNT(*)
-// LIMIT 5;
--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -1,35 +1,62 @@
 <script setup>
-import { reactive } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import cards from '../components/cards.vue';
+import axios from 'axios';
+import Cards from '../components/cards.vue';
 
 let model = reactive({
   searchbar: ''
 })
+
+let accommodations = ref([])
+
+onMounted(()=>
+{
+  axios.get("http://localhost:3000/accommodations/top5")
+    .then(data=>
+    {
+      accommodations.value = data.data;
+      console.log(accommodations.value);
+    })
+    .catch(err=>
+    {
+      console.error(err);
+    })
+})
+
 </script>
 
 <template>
   <div class="home">
     <div class="container">
-    <div class="row justify-content-center">
-      <h1 class="display-1 text-white text-center w-100">
-        Hová szeretnél utazni?
-      </h1>
-      <form class="form mt-5 col-12 col-lg-4 col-md-5 row justify-content-center 
-                   bg-dark bg-opacity-50 rounded-3 shadow
-                   p-4 border border-1 border-white">
+      <div class="row justify-content-center">
+        <h1 class="display-1 text-white text-center w-100">
+          Hová szeretnél utazni?
+        </h1>
+        <form class="form mt-5 col-12 col-lg-4 col-md-5 row justify-content-center 
+                    bg-dark bg-opacity-50 rounded-3 shadow
+                    p-4 border border-1 border-white">
 
-        <!-- Keresési mező -->
-        <input  class="form-control mt-2" 
-                type="search" 
-                name="searchbar" 
-                id="searchbar"
-                placeholder="Írja be a település nevét. Pl.: Tokyo"
-                v-model="model.searchbar">
-        <!-- Keresési gomb -->
-        <button class="btn btn-outline-light col-11 my-2 mt-3"
-                :disabled="!model.searchbar">Keresés</button>
-      </form> 
-    </div>
+          <!-- Keresési mező -->
+          <input  class="form-control mt-2" 
+                  type="search" 
+                  name="searchbar" 
+                  id="searchbar"
+                  placeholder="Írja be a település nevét. Pl.: Tokyo"
+                  v-model="model.searchbar">
+          <!-- Keresési gomb -->
+          <button class="btn btn-outline-light col-11 my-2 mt-3"
+                  :disabled="!model.searchbar">Keresés</button>
+        </form> 
+      </div>
+      <div v-for="x in accommodations">
+        <Cards 
+          table-name="accommodations"
+          :country_id="x.country_id"
+          :country_name="x.name"
+          page="home"
+        />
+      </div>
     </div>
   </div>
 </template>
