34ae291d467ed85e112e8f3d315f9173f86b914c
HMate0317 <mateherczeg740@gmail.com>
2026-01-14 09:07:23 +0100

    comment

--- frontend/src/pages/settings.vue
+++ frontend/src/pages/settings.vue
@@ -4,12 +4,15 @@ import axios from 'axios';
 import { reactive,ref } from 'vue';
 let currencyOption = ref(selectedCurrency)
 let currencys = ref([]),
+		//Definiálom a 'setCurrency' metódust
 		setCurrency = () => {
+			// Megváltoztatjuk a selectedCurrency értékeit
+			// Ha megváltozik akkor a localstorage-ba elementi az értékeket(store/currency.js)
 			selectedCurrency.currencyID = currencyOption.value.id;
 			selectedCurrency.currencyMultiplier = currencyOption.value.multiplier;
 			selectedCurrency.currencyName = currencyOption.value.name;
 			selectedCurrency.currencyShortedName = currencyOption.value.shorted_name;
-		}
+		};
 
 axios.get('http://localhost:3000/getCurrency')	
 .then(response => {
@@ -17,6 +20,8 @@ axios.get('http://localhost:3000/getCurrency')
 	console.log(response.data)
 })
 .catch(e => console.error(e))
+
+
 </script>
 
 <template>
@@ -24,23 +29,25 @@ axios.get('http://localhost:3000/getCurrency')
 		<div>
 			<h1 class="display-1 text-center text-white">Beállítások</h1>
 		</div>
-
+		<!-- Valuta választó menüpont -->
 		<div class="row text-white border border-white 
 								border-1 mx-5 rounded-3">
 			<h1 class="display-4">Valuta választása</h1>
 
+			<!-- Select -->
 			<select class="form-select bg-dark text-white 
 										 my-2 w-auto text-center mx-auto"
 							v-on:change="setCurrency()"
 							v-model="currencyOption"
-							id="currencySelector"
-							>
+							id="currencySelector">
+
+				<!-- Frissítés esetén írja a kiválaszott currency-t -->
 				<option selected 
 								:value="selectedCurrency"
 								disabled> 
 								{{selectedCurrency.currencyName}}
 				</option>
-
+				<!-- Választékok a currencyk közül -->
 				<option v-for="x in currencys" 
 								:value="x">
 						{{ x.name}}
