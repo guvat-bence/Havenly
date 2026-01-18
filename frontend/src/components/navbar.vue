ed8ebcfe056913d89b726f9b174bb5b746a44d2e
HMate0317 <mateherczeg740@gmail.com>
2026-01-18 17:09:51 +0100

    tagging

--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -52,7 +52,7 @@ let mainRoutes = [
 		for (let index = 0; index < activeLocations.value.length; index++) {
 
 			if ((convertStrings(activeLocations.value[index].city_name)).split(" ")
-				.filter(x => x.includes(convertStrings(value))).length > 0) {
+				  .filter(x => x.includes(convertStrings(value))).length > 0) {
 
 				result.value.push(activeLocations.value[index])
 			}
