49710e657c230c7c97330668dc868e35fcecc011
HMate0317 <mateherczeg740@gmail.com>
2026-02-09 17:18:00 +0100

    form design

--- frontend/src/pages/basket.vue
+++ frontend/src/pages/basket.vue
@@ -1,52 +1,157 @@
 <script setup>
 import { rent } from '@/store/current_rent';
+import { user } from '@/store/user';
 
 console.log(rent);
+console.log(user)
 </script>
 <template>
   <div class="container">
     <h1 class="display-1 text-center text-center text-white">Kosár</h1>
-
+    <!-- Billing adresses -->
     <div class="row">
       <button class="btn btn-outline-light mx-auto w-auto mb-5 mt-3" 
               type="button" 
               data-bs-toggle="collapse"
-              data-bs-target="#billingCollapse" aria-expanded="false" aria-controls="billingCollapse">
+              data-bs-target="#billingCollapse" 
+              aria-expanded="false" 
+              aria-controls="billingCollapse">
         Számlázási adatok
       </button>
 
-      <div class="collapse" id="billingCollapse">
-        <div class="card bg-transparent text-white border-white text-black card-body">
+      <div class="collapse show" id="billingCollapse">
+        <div class="card bg-black bg-opacity-25 w-75 mx-auto text-white border-white text-black card-body">
           <form>
-            <!-- LABEL SOR -->
+
+            <!-- Names -->
             <div class="row mb-3">
-              <div class="col-4">
-                <label for="InputFirstName" class="form-label">Keresztnév:</label>
+              <div>
+                <h5 class="text-center">Nevek</h5>
               </div>
-              <div class="col-4">
-                <label for="InputLastName" class="form-label">Vezetéknév:</label>
+              <!-- FirstName -->
+              <div class="mb-3 m-0 col-12 col-lg-4">
+                <label for="InputFirstName" 
+                       class="form-label">
+                  Keresztnév
+                </label>
+                <input type="text" 
+                       class="form-control" 
+                       id="InputFirstName">
               </div>
 
-              <div class="col-4">
-                <label for="InputLastName" class="form-label">Másodiknév:</label>
+              <!-- LastName -->
+              <div class="mb-3 col-12 col-lg-4">
+                <label for="InputLastName" 
+                       class="form-label">
+                  Vezetéknév
+                </label>
+                <input type="text" 
+                       class="form-control" 
+                       id="InputLastName">
+              </div>
+
+              <!-- MiddleName -->
+              <div class="mb-3 col-12 col-lg-4">
+                <label for="InputMiddleName" 
+                       class="form-label">
+                  Harmadiknév
+                </label>
+                <input type="text" 
+                       class="form-control" 
+                       id="InputMiddleName">
               </div>
             </div>
 
-            <!-- INPUT SOR -->
-            <div class="row mb-3">
-              <div class="col-4">
-                <input type="text" class="bg-transparent border-0 border-bottom text-white w-100" id="InputFirstName"
-                  placeholder="Keresztnév">
+            <!-- Contact information -->
+            <div class="row">
+              <div>
+                <h5 class="text-center">Elérési módok</h5>
               </div>
 
-              <div class="col-4">
-                <input type="text" class="bg-transparent border-0 border-bottom text-white w-100" id="InputLastName"
-                  placeholder="Vezetéknév">
+              <!-- Email -->
+              <div class="mb-3 col-6 col-lg-6">
+                <label for="InputEmail" 
+                       class="form-label">
+                  Email
+                </label>
+                <input type="email" 
+                       class="form-control" 
+                       id="InputEmail">
               </div>
 
-              <div class="col-4">
-                <input type="text" class="bg-transparent border-0 border-bottom text-white w-100" id="InputMiddleName"
-                  placeholder="Másodiknév">
+              <!-- PhoneNumber -->
+              <div class="mb-3 col-6 col-lg-6">
+                <label for="InputPhoneNum" 
+                       class="form-label">
+                  Telefonszám
+                </label>
+                <input type="text" 
+                       class="form-control" 
+                       id="InputPhoneNum">
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <!-- Payment method -->
+    <div class="row">
+      <button class="btn btn-outline-light mx-auto w-auto mb-5 mt-3" 
+              type="button" 
+              data-bs-toggle="collapse"
+              data-bs-target="#methodCollapse" 
+              aria-expanded="false" 
+              aria-controls="methodCollapse">
+        Fizetési mód
+      </button>
+
+      <div class="collapse show" id="methodCollapse">
+        <div class="card bg-black bg-opacity-25 w-50 mx-auto text-white border-white text-black card-body">
+          <form>
+            <div class="row">
+
+              <!-- Cash -->
+              <div class="form-check d-flex justify-content-between 
+                          align-items-center mb-2">
+                <label class="form-check-label h5" 
+                       for="radioCash">
+                  Készpénz
+                </label>
+                <input class="form-check-input" 
+                       type="radio" 
+                       name="radioDefault" 
+                       id="radioCash">
+              </div>
+
+              <hr>
+
+              <!-- CreditCard -->
+              <div class="form-check d-flex justify-content-between 
+                          align-items-center">
+                <label class="form-check-label h5" 
+                       for="radioCard">
+                  Bankkártya
+                </label>
+                <input class="form-check-input" 
+                       type="radio" 
+                       name="radioDefault" 
+                       id="radioCard">
+              </div>
+
+              <hr>
+
+              <!-- Szépcard -->
+              <div class="form-check d-flex justify-content-between 
+                          align-items-center">
+                <label class="form-check-label h5" 
+                       for="radioPayPal">
+                  PayPal
+                </label>
+                <input class="form-check-input" 
+                       type="radio" 
+                       name="radioDefault" 
+                       id="radioPayPay">
               </div>
             </div>
           </form>
@@ -58,5 +163,15 @@ console.log(rent);
 <style>
 input {
   outline: none;
+}
+
+.custom-width{
+  width: 100%;
+}
+
+@media (min-width: 900px) {
+  .w-md-50{
+    width: 50% !important;
+  }
 }
 </style>
