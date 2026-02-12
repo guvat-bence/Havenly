78c9682b33dbbd3a463e2a231bc883150399e056
HMate0317 <mateherczeg740@gmail.com>
2026-02-12 09:43:11 +0100

    get inforamtion

--- frontend/src/pages/basket.vue
+++ frontend/src/pages/basket.vue
@@ -7,6 +7,7 @@ import { reactive } from 'vue';
 
 let accommodation_data = reactive(JSON.parse(rent.accommodation));
 
+console.log(user.cardNumber)
 
 if (!accommodation_data.id)
   router.back()
@@ -19,6 +20,10 @@ let model = reactive({
   phoneNum: user.phone_number
 })
 
+let card = reactive({
+  cardnumber: user.cardNumber === null ? ""  : user.cardNumber,
+  expiration: user.expiration,
+}) 
 let convertStrings = (str) => {
       
     	
@@ -92,11 +97,9 @@ let convertStrings = (str) => {
 
       <!-- user datas -->
       <div class="col-12 col-md-6">
-
         <!-- Billing adresses -->
         <div class="row">
           <h4 class="text-white mx-auto w-auto mb-5 mt-3" 
-              type="button" 
               data-bs-toggle="collapse"
               data-bs-target="#billingCollapse"
               aria-controls="billingCollapse">
@@ -188,9 +191,8 @@ let convertStrings = (str) => {
         </div>
 
         <!-- Payment method -->
-        <div class="row justify-content-lg-start">
+        <div class="row">
           <h4 class="text-white mx-auto w-auto mb-5 mt-3" 
-              type="button" 
               data-bs-toggle="collapse"
               data-bs-target="#methodCollapse" 
               aria-controls="methodCollapse">
@@ -198,7 +200,7 @@ let convertStrings = (str) => {
           </h4>
 
           <div class="collapse show" 
-               id="methodCollapse">
+              id="methodCollapse">
             <div class="card bg-black bg-opacity-25 w-50 
                         mx-auto text-white border-white text-black 
                         card-body">
@@ -209,13 +211,15 @@ let convertStrings = (str) => {
                   <div class="form-check d-flex justify-content-between 
                               align-items-center mb-2">
                     <label class="form-check-label h5" 
-                           for="radioCash">
+                          for="radioCash">
                       Készpénz
                     </label>
                     <input class="form-check-input" 
-                           type="radio" 
-                           name="radioDefault" 
-                           id="radioCash">
+                          type="radio" 
+                          name="radioDefault" 
+                          id="radioCash"
+                          value="cash"
+                          v-model="model.method">
                   </div>
 
                   <hr>
@@ -224,13 +228,15 @@ let convertStrings = (str) => {
                   <div class="form-check d-flex justify-content-between 
                             align-items-center">
                     <label class="form-check-label h5" 
-                           for="radioCard">
+                          for="radioCard">
                       Bankkártya
                     </label>
                     <input class="form-check-input" 
-                           type="radio" 
-                           name="radioDefault" 
-                           id="radioCard">
+                          type="radio" 
+                          name="radioDefault" 
+                          id="radioCard"
+                          value="creditcard"
+                          v-model="model.method">
                   </div>
 
                   <hr>
@@ -239,17 +245,110 @@ let convertStrings = (str) => {
                   <div class="form-check d-flex justify-content-between 
                             align-items-center">
                     <label class="form-check-label h5" 
-                           for="radioPayPal">
+                          for="radioPayPal">
                       PayPal
                     </label>
                     <input class="form-check-input" 
-                           type="radio" 
-                           name="radioDefault" 
-                           id="radioPayPay">
+                          type="radio" 
+                          name="radioDefault" 
+                          id="radioPayPay"
+                          value="paypal"
+                          v-model="model.method">
                   </div>
                 </div>
               </form>
             </div>
+          </div>
+        </div>
+
+        <div>
+          <div class="row"
+              v-if="model.method == 'creditcard' || 
+                    model.method == 'paypal'">
+              <h4 class="text-center text-white mt-4"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#cardDataCollapse" 
+                  aria-controls="cardDataCollapse">
+                Kártya adatok
+              </h4>
+
+              <div id="cardDataCollapse"
+                  class="collapse show">
+                <div class="bg-black bg-opacity-25 text-center 
+                            text-white border-1 border-white 
+                            card w-auto mx-auto text-white p-2">
+                  <form v-if="model.method == 'creditcard'">
+
+                    <!-- FirstName -->
+                    <h4>Kártya tulajdonos neve</h4>
+                    <div class="row">
+                      <div class="mb-3 m-0 col-12 col-lg-4">
+                        <label for="InputFirstName" 
+                              class="form-label">
+                          Keresztnév
+                        </label>
+                        <input type="text" 
+                               class="form-control" 
+                               id="InputFirstName" 
+                               v-model="model.firstName">
+                      </div>
+
+                      <!-- LastName -->
+                      <div class="mb-3 m-0 col-12 col-lg-4">
+                        <label for="InputFirstName" 
+                              class="form-label">
+                          Vezetéknév
+                        </label>
+                        <input type="text" 
+                               class="form-control" 
+                               id="InputFirstName" 
+                               v-model="model.lastName">
+                      </div>
+
+                      <!-- MiddleName -->
+                      <div class="mb-3 m-0 col-12 col-lg-4">
+                        <label for="InputFirstName" 
+                              class="form-label">
+                          Harmadiknév
+                        </label>
+                        <input type="text" 
+                               class="form-control" 
+                               id="InputFirstName" 
+                               v-model="model.middleName">
+                      </div>
+                    </div>
+
+                    <hr>
+
+                    <!-- card data -->
+                    <div class="row">
+                      <!-- cardNumber -->
+                      <div class="mb-3 m-0 col-12">
+                        <label for="inputCardNumber" 
+                              class="form-label">
+                          Kártyaszám
+                        </label>
+                        <input type="text" 
+                               class="form-control" 
+                               id="inputCardNumber" 
+                               v-model="card.cardnumber">
+                      </div>
+
+                      <!-- card expirationDate and cvv  -->
+                      <div class="mb-3 m-0 col-12">
+                        <label for="inputCardNumber" 
+                              class="form-label">
+                          Kártyaszám
+                        </label>
+                        <input type="text" 
+                               class="form-control" 
+                               id="inputCardNumber" 
+                               v-model="card.expiration">
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
           </div>
         </div>
       </div>
