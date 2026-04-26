a0f7384d1d06473def20268a025d50f535d7699e
HMate0317 <mateherczeg740@gmail.com>
2026-04-26 21:54:25 +0200

    installer

--- wpf/Havenly/obj/Debug/net8.0-windows7.0/App.g.cs
+++ wpf/Havenly/obj/Debug/net8.0-windows7.0/App.g.cs
@@ -47,7 +47,7 @@ namespace Havenly {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace Havenly {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
         public static void Main() {
             Havenly.App app = new Havenly.App();
             app.InitializeComponent();
