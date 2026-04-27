a1129b7bd2f26b316bdd3e1831aec32e42107ace
guvat-bence <guvat.bence-2021@keri.mako.hu>
2026-04-27 11:22:50 +0200

    Fix merge

--- wpf/src/js/getLocation.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-import { ref, watch } from "vue"
-export let searchInput = ref("")
-export let activeLocations = ref([])
-
-watch(activeLocations, (value) => activeLocations.value = value)
